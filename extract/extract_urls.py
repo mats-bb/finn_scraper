ad4e6a4aa433c149c2c217bf31d416c008654629
mats-bb <mats.k.berg@gmail.com>
2024-02-14 14:04:01 +0100

    Changed base url to fetch 'todays' joblistings

--- extract/extract_urls.py
+++ extract/extract_urls.py
@@ -2,18 +2,18 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 import json
 
-base_url = fr"https://www.finn.no/job/fulltime/search.html?location=1.20001.20061"
+base_url = fr"https://www.finn.no/job/fulltime/search.html?location=1.20001.20061&published=1"
 
 def save_to_json(urls):
     with open('urls.json', 'w') as f:
         json.dump(urls, f, indent=4)
 
-def get_first_page(first_page_url):
+def get_first_page(base_url):
     urls = []
 
-    r = req.get(first_page_url)
-    if r.status_code == 200:
-        soup = bs(r.content, "html.parser")
+    resp = req.get(base_url)
+    if resp.status_code == 200:
+        soup = bs(resp.content, "html.parser")
         classes = soup.find_all(class_='sf-search-ad-link link link--dark hover:no-underline')
         for class_ in classes:
             urls.append(class_['href'])
@@ -25,19 +25,16 @@ def get_conseq_pages(first_page_url, urls):
 
     while True:
     
-        r = req.get(fr"{first_page_url}" + "&page=" + str(page_num))
+        resp = req.get(fr"{first_page_url}" + "&page=" + str(page_num))
 
-        if r.status_code == 200:
-            soup = bs(r.content, "html.parser")
+        if resp.status_code == 200:
+            soup = bs(resp.content, "html.parser")
             classes = soup.find_all(class_='sf-search-ad-link link link--dark hover:no-underline')
-
-            print(len(classes))
 
             for class_ in classes:
                 urls.append(class_['href'])
 
             page_num += 1
-            print(page_num)
                 
         else:
             break
