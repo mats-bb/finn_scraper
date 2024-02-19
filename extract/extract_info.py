fbb05cf7cc19cb7d71596691dd31d3c6f815d1b8
mats-bb <mats.k.berg@gmail.com>
2024-02-19 15:49:14 +0100

    started refactoring process

--- extract/extract_info.py
+++ extract/extract_info.py
@@ -8,10 +8,51 @@ sys.path.insert(1, root)
 from helper_funcs.helpers import get_resp, get_soup, load_from_json, save_to_json
 
 def clean_words(list_):
+    """Remove unwanted punctuation and whitespace."""
     list_ = [word.rstrip(',') for word in list_]
     list_ = [word.strip() for word in list_]
 
     return list_
+
+def add_info(terms_dict, url):
+    terms_dict["url"] = url
+    terms_dict["date_added"] = str(datetime.today().date())
+
+def get_tags_list(term, soup):
+    tags_list = []
+    try:
+        tag_info = soup.find("dt", string=term).find_next_siblings()
+    except ValueError:
+        tags_list.append("Missing")
+    else:
+        for tag in tag_info:
+            if tag.name == "dt":
+                break
+            else:
+                tags_list.append(tag.text)
+
+    return tags_list
+
+def process_special_terms(term, tags_list):
+    if term in ("Stillingsfunksjon", "Bransje"):
+        if len(tags_list) == 1 and ',' in tags_list[0]:
+                tags_list = tags_list[0].split(',')
+
+    return tags_list
+
+def assign_term_to_dict(terms_dict, term, tags_list):
+    if len(tags_list) == 1:
+        terms_dict[term] = str(tags_list[0])
+    else:
+        terms_dict[term] = tags_list
+
+    return terms_dict
+
+def extract_url_info(urls):
+    dict_list = []
+
+    for url in urls:
+        pass
 
 def get_term_words(terms, soup):
     terms_dict = {}
@@ -25,7 +66,6 @@ def get_term_words(terms, soup):
             tags_list.append("Missing")
         else:
 
-
             for tag in tag_info:
                 if tag.name == "dt":
                     break
@@ -36,11 +76,6 @@ def get_term_words(terms, soup):
                     
             if len(tags_list) == 1 and ',' in tags_list[0]:
                 tags_list = tags_list[0].split(',')
-
-
-            # elif len(tags_list) == 1 and ',' in tags_list[0]:
-            #     tags_list = tags_list[0].split(',')
-            #     tags_list = clean_words(tags_list)
                 
         tags_list = clean_words(tags_list)
                 
